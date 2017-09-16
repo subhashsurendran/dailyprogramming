5772a8a36baac10300084b1e1ccb8174933a79a9
Subhash Surendran <gosubhash31@gmail.com>
2017-09-16 16:40:30 -0500

    Full implementation

--- MergeSort/src/main/java/com/subhash/daily/programming/MergeSort/MergeSortImpl.java
+++ MergeSort/src/main/java/com/subhash/daily/programming/MergeSort/MergeSortImpl.java
@@ -1,54 +1,84 @@
 package com.subhash.daily.programming.MergeSort;
 
-/**
- * Hello world!
- *
- */
-public class MergeSortImpl 
-{
-       
-    public static void main(String args[])
-    {
-        int arr[] = {12, 11, 13, 5, 6, 7};
- 
-        System.out.println("Given Array");
-        printArray(arr);
- 
-        MergeSortImpl ob = new MergeSortImpl();
-        ob.mergeSort(arr, 0, arr.length-1);
- 
-        System.out.println("\nSorted array");
-        printArray(arr);
-    }
-    
-    
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-    
-    private void mergeSort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            // Find the middle point
-            int m = (l+r)/2;
- 
-            // Sort first and second halves
-            mergeSort(arr, l, m);
-            mergeSort(arr , m+1, r);
- 
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
+import org.apache.log4j.Logger;
 
-	private void merge(int[] mainArray,  int leftIndex, int i, int rightIndex) {
-		
-		
-		
+public class MergeSortImpl {
+
+	final static Logger logger = Logger.getLogger(MergeSortImpl.class);
+
+	public static void main(String args[]) {
+		int arr[] = { 12, 11, 13, 5, 6, 7 };
+
+		logger.debug("Program Started");
+
+		MergeSortImpl ob = new MergeSortImpl();
+		ob.mergeSort(arr, 0, arr.length - 1);
+
+		logger.debug("\n Sorted array");
+		printArray(arr);
+	}
+
+	static void printArray(int arr[]) {
+		int n = arr.length;
+		logger.debug("Array length is :" + n);
+		for (int i = 0; i < n; ++i) {
+
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	private void mergeSort(int arr[], int leftEnd, int rightEnd) {
+		if (leftEnd < rightEnd) {
+			int m = (leftEnd + rightEnd) / 2;
+
+			mergeSort(arr, leftEnd, m);
+			mergeSort(arr, m + 1, rightEnd);
+
+			merge(arr, leftEnd, m, rightEnd);
+		}
+	}
+
+	private void merge(int arr[], int leftIndex, int mid, int rightIndex) {
+		{
+			int n1 = mid - leftIndex + 1;
+			int n2 = rightIndex - mid;
+
+			int leftArr[] = new int[n1];
+			int rightArr[] = new int[n2];
+
+			for (int i = 0; i < n1; ++i) {
+				leftArr[i] = arr[leftIndex + i];
+			}
+			for (int j = 0; j < n2; ++j) {
+				rightArr[j] = arr[mid + 1 + j];
+			}
+
+			int i = 0, j = 0;
+
+			int k = leftIndex;
+			while (i < n1 && j < n2) {
+				if (leftArr[i] <= rightArr[j]) {
+					arr[k] = leftArr[i];
+					i++;
+				} else {
+					arr[k] = rightArr[j];
+					j++;
+				}
+				k++;
+			}
+
+			while (i < n1) {
+				arr[k] = leftArr[i];
+				i++;
+				k++;
+			}
+
+			while (j < n2) {
+				arr[k] = rightArr[j];
+				j++;
+				k++;
+			}
+		}
+
 	}
 }
