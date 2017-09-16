bdbb08947392b6beed5337d218ce02c39c31194a
Subhash Surendran <gosubhash31@gmail.com>
2017-09-16 14:24:55 -0500

    Log4J

--- MergeSort/src/main/java/com/subhash/daily/programming/MergeSort/MergeSortImpl.java
+++ MergeSort/src/main/java/com/subhash/daily/programming/MergeSort/MergeSortImpl.java
@@ -6,8 +6,49 @@ package com.subhash.daily.programming.MergeSort;
  */
 public class MergeSortImpl 
 {
-    public static void main( String[] args )
+       
+    public static void main(String args[])
     {
-        System.out.println( "Hello World!" );
+        int arr[] = {12, 11, 13, 5, 6, 7};
+ 
+        System.out.println("Given Array");
+        printArray(arr);
+ 
+        MergeSortImpl ob = new MergeSortImpl();
+        ob.mergeSort(arr, 0, arr.length-1);
+ 
+        System.out.println("\nSorted array");
+        printArray(arr);
     }
+    
+    
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    
+    private void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            // Find the middle point
+            int m = (l+r)/2;
+ 
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr , m+1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+	private void merge(int[] mainArray,  int leftIndex, int i, int rightIndex) {
+		
+		
+		
+	}
 }
--- MergeSort/src/test/java/com/subhash/daily/programming/MergeSort/MergeSortImplTest.java
+++ MergeSort/src/test/java/com/subhash/daily/programming/MergeSort/MergeSortImplTest.java
@@ -1,8 +1,18 @@
 package com.subhash.daily.programming.MergeSort;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class MergeSortImplTest extends TestCase {
+	
+	
+	@Test
+	public void testMain() {
+		
+		MergeSortImpl.main(null);
+		assertTrue(true);
+	}
 	
 	
 
